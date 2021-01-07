759208784d7444dfe500ee62c5561b213e7a3c74
Ritam42 <73995398+Ritam42@users.noreply.github.com>
2021-01-07 01:52:09 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock, dogImg;
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  dog = createSprite(250, 250, 20, 20);
+  dog.addImage(dogImg);
+
+  database = firebase.database();
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+  
+}
+
+function draw() {  
+   
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+   
+  drawSprites();
+  //add styles here
+  text("Note: Press UP_ARROW key to feed Drago Milk!", 250, 30);
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+}
